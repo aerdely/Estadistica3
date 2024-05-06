adfa56d1424cff47854419c7ce64e66929e0a5ff
Arturo Erdély <arturoerdely@gmail.com>
2024-05-06 16:12:17 -0600

    Update 00Stats3.jl

--- src/00Stats3.jl
+++ src/00Stats3.jl
@@ -87,6 +87,153 @@ function Tn(interval::String, xobs::Vector{<:Real})
     return tn
 end
 
+# EDA
+"""
+    EDA(fobj, valmin, valmax; iEnteros = zeros(Int, 0), tamgen = 1000, propselec = 0.3, difmax = 0.00001, maxiter = 1000)
+
+`fobj` A real function of several variables to be minimized, where its argument is a vector or 1-D array.
+
+`valmin, valmax` vectors or 1-D arrays of minimum and maximum values for the first generation.
+
+`iEnteros` index of variables that must take integer values.
+
+`tamgen` size of each generation.
+
+`propselec` proportion of population to be selected.
+
+`difmax` error tolerance.
+
+`maxiter` maximum number of iterations.
+
+# Example 1
+```
+f(x) = (x[1] - 5)^4 - 16(x[1] - 5)^2 + 5(x[1] - 5) + 120
+EDA(f, [0], [9])
+```
+
+# Example 2
+This non-negative function is clearly minimized at (5,-2).
+```
+f(z) = abs(z[1] - 5) + abs(z[2] + 2)
+EDA(f, [-10, -10], [10, 10])
+```
+
+# Example 3
+The same function but only allowing integer values:
+```
+EDA(f, [-10, -10], [10, 10], iEnteros = [1, 2])
+```
+"""
+function EDA(fobj, valmin, valmax; iEnteros = zeros(Int, 0), tamgen = 1000,
+             propselec = 0.3, difmax = 0.00001, maxiter = 1000)
+    numiter = 1
+    println("Iterando... ")
+    numvar = length(valmin)
+    nselec = Int(round(tamgen * propselec))
+    G = zeros(tamgen, numvar)
+    Gselec = zeros(nselec, numvar)
+    for j ∈ 1:numvar
+        G[:, j] = valmin[j] .+ (valmax[j] - valmin[j]) .* rand(tamgen)
+    end
+    if length(iEnteros) > 0
+        for j ∈ iEnteros
+            G[:, j] = round.(G[:, j])
+        end
+    end
+    d(x, y) = sqrt(sum((x .- y) .^ 2))
+    rnorm(n, μ, σ) = μ .+ (σ .* randn(n))
+    promedio(x) = sum(x) / length(x)
+    desvest(x) = sqrt(sum((x .- promedio(x)) .^ 2) / (length(x) - 1))
+    fG = zeros(tamgen)
+    maxGselec = zeros(tamgen)
+    minGselec = zeros(tamgen)
+    media = zeros(numvar)
+    desv = zeros(numvar)
+    while numiter < maxiter
+        # evaluando función objetivo en generación actual:
+        print(numiter, "\r")
+        for i ∈ 1:tamgen
+            fG[i] = fobj(G[i, :])
+        end
+        # seleccionando de generación actual:
+        umbral = sort(fG)[nselec]
+        iSelec = findall(fG .≤ umbral)
+        Gselec = G[iSelec, :]
+        for j ∈ 1:numvar
+            maxGselec[j] = maximum(Gselec[:, j])
+            minGselec[j] = minimum(Gselec[:, j])
+            media[j] = promedio(Gselec[:, j])
+            desv[j] = desvest(Gselec[:, j])
+        end
+        # salir del ciclo si se cumple criterio de paro:
+        if d(minGselec, maxGselec) < difmax 
+            break
+        end
+        # y si no se cumple criterio de paro, nueva generación:
+        numiter += 1
+        for j ∈ 1:numvar
+            G[:, j] = rnorm(tamgen, media[j], desv[j])
+        end
+        if length(iEnteros) > 0
+            for j ∈ iEnteros
+                G[:, j] = round.(G[:, j])
+            end
+        end
+    end
+    println("...fin")
+    fGselec = zeros(nselec)
+    for i ∈ 1:length(fGselec)
+        fGselec[i] = fobj(Gselec[i, :])
+    end
+    xopt = Gselec[findmin(fGselec)[2], :]
+    if length(iEnteros) > 0
+        for j ∈ iEnteros
+            xopt[j] = round(xopt[j])
+        end
+    end
+    fxopt = fobj(xopt)
+    r = (x = xopt, fx = fxopt, iter = numiter)
+    if numiter == maxiter
+        println("Aviso: se alcanzó el máximo número de iteraciones = ", maxiter)
+    end
+    return r
+end
+
+# Bn
+using Distributions
+"""
+    Bn(interval::String, obs, γ = 0.95)
+
+Nonparametric point and 100γ% probability interval estimation for the probability of an `interval` using the observed random sample in a vector `obs` and the bayesian paradigm (g = 0.95 default value if not specified). `interval` is specified a string, where the first and last characters must be an open or closed bracket, that is `]` or `[` and the left and right extremes must be numbers separated by a comma.
+
+Dependencies:
+- internal: `Tn` `EDA`
+- external: `Distributions.jl`
+
+# Example
+```
+W = Normal(-2, 3)
+cdf(W, 3) - cdf(W, 0) # P(0 < W < 3) 
+wobs = rand(W, 1_000);
+Bn("]0,3[", wobs, 0.99)
+```
+"""
+function Bn(interval::String, obs, γ = 0.95)
+    # using: Distributions.jl
+    # Dependencies: Tn, EDA
+    n = length(obs)
+    tn = Tn(interval, obs)
+    α, β = 1 + n*tn, 1 + n*(1 - tn) # posterior parameters
+    Θ = Beta(α, β) # posterior distribution
+    θmedia, θmediana = mean(Θ), median(Θ)
+    h(z) = (quantile(Θ, γ + cdf(Θ, z[1])) - z[1]) * Inf^(z[1] > quantile(Θ, 1 - γ))
+    sol = EDA(h, [0], [quantile(Θ, 1- γ)])
+    θ₁ = sol[1][1]
+    θ₂ = quantile(Θ, γ + cdf(Θ, θ₁))
+    estimación = (insesgado = tn, media = θmedia, mediana = θmediana, intervalo = (θ₁, θ₂))
+    return estimación
+end
+
 # cuantil_puntual
 """
     cuantil_puntual(α::Real, obs::Vector{<:Real})
@@ -98,7 +245,7 @@ Empirical `α` quantile point estimation using the observed random sample in a v
 cuantil_puntual(0.97725, randn(100000)) # theoretically it's 2.0 approx
 ```
 """
-function cuantil(α::Real, xobs::Vector{<:Real})
+function cuantil_puntual(α::Real, xobs::Vector{<:Real})
     obs = sort(xobs)
     n = length(obs)
     obsmin = minimum(obs)
@@ -124,7 +271,6 @@ function cuantil(α::Real, xobs::Vector{<:Real})
     cuantil = (j + 1  - orden)*obs[j] + (orden - j)*obs[j+1]
     return cuantil
 end
-
 
 # GoF
 """
@@ -177,13 +323,197 @@ function GoF(xobs, Fo, simFo; prueba = "AD", numsims = 100_000, msg = true)
     return pvalue
 end
 
+# Poligonal
+"""
+    poligonal(x::Vector{<:Real}, xobs::Vector{<:Real}; mínimo = minimum(xobs), máximo = maximum(xobs))
+
+Polygonal approximation of a continuous distribution function at a given vector of values `x` based
+on an observed random sample given by vector `xobs`. The optional parameter `mínimo` may be set to a value
+smaller than the sample minimum, and `máximo` to a greater value than the sample maximum, if required.
+
+## Example
+```
+xobs = randn(10_000)
+x = [-1.96, 0, 1.96]
+poligonal(x, xobs)
+```
+"""
+function poligonal(x::Vector{<:Real}, xobs::Vector{<:Real}; mínimo = minimum(xobs), máximo = maximum(xobs))
+    n = length(xobs)
+    xord = sort(xobs)
+    xp = (xord[1:(n-1)] .+ xord[2:n]) ./ 2
+    xp = vcat(mínimo, xp, máximo)
+    function g(z, xp)
+        a = 0.0
+        if z > xp[n+1]
+            a = 1.0
+        elseif z > xp[1]
+            k = findmax(z .≤ xp)[2]
+            a = (1/n) * ((k-1) - (xp[k] - z)/(xp[k] - xp[k-1]))
+        end
+        return a
+    end
+    m = length(x)
+    polivalores = zeros(m)
+    for i ∈ 1:m
+        polivalores[i] = g(x[i], xp)
+    end
+    return polivalores 
+end
+
+# poligonal_cuantiles
+"""
+    poligonal_cuantiles(u::Vector{<:Real}, xobs::Vector{<:Real}; mínimo = minimum(xobs), máximo = maximum(xobs))
+
+Polygonal approximation of the quantile function of a continuous distribution given vector of values `0 < u < 1`
+based on an observed random sample given by vector `xobs`. The optional parameter `mínimo` may be set to a value
+smaller than the sample minimum, and `máximo` to a greater value than the sample maximum, if required.
+
+## Example
+```
+xobs = randn(10_000)
+u = [0.025, 0.5, 0.975]
+poligonal_cuantiles(u, xobs)
+```
+"""
+function poligonal_cuantiles(u::Vector{<:Real}, xobs::Vector{<:Real}; mínimo = minimum(xobs), máximo = maximum(xobs))
+    n = length(xobs)
+    xord = sort(xobs)
+    xp = (xord[1:(n-1)] .+ xord[2:n]) ./ 2
+    xp = vcat(mínimo, xp, máximo)
+    function g(z, xp)
+        if 0 < z < 1
+            k = findmax(n*z .≤ collect(1:n))[2]
+            return xp[k+1] - (xp[k+1] - xp[k])*(k - n*z)
+        else
+            return NaN 
+        end
+    end
+    m = length(u)
+    cuantiles = zeros(m)
+    for i ∈ 1:m
+        cuantiles[i] = g(u[i], xp)
+    end
+    return cuantiles
+end
+
+
+# Algoritmo de bisección
+"""
+    biseccion(f, a, b; δ = abs((a + b)/2) / 1_000_000, m = 10_000)
+
+`f` una función continua sobre el intervalo cerrado `[a,b]` tal que `f(a)f(b)<0`
+con un error máximo de aproximación `δ` a un valor `c` tal que `f(c)=0` en un
+número máximo de iteraciones `m` (método de bisección)
+
+## Ejemplo
+```
+f(x) = (x - 3) * (x - 1) * (x + 1)
+biseccion(f, -1.9, 0)
+```
+"""
+function biseccion(f, a, b; δ = abs((a + b)/2) / 1_000_000, m = 10_000)
+    iter = 1
+    z = (a + b) / 2
+    while iter ≤ m
+        if f(z) == 0.0 || (b - a)/2 ≤ δ
+            break # criterio de paro
+        elseif f(a) * f(z) > 0
+            a = z
+        else
+            b = z
+        end
+        z = (a + b) / 2
+        iter += 1
+    end
+    iter -= 1
+    if iter == m
+        println("Se alcanzó el número máximo de iteraciones = $m")
+    end
+    return (raíz = z, dif = f(z), numiter = iter, maxiter = m, tol = δ)
+end
+
+
+# Bernstein_cuantiles
+"""
+    Bernstein_cuantiles(u::Vector{<:Real}, xobs::Vector{<:Real})
+
+Bernstein polynomials approximation of the quantile function of a continuous distribution given vector of
+values `0 < u < 1` based on an observed random sample given by vector `xobs`.
+
+Dependencies:
+- external: `Distributions.jl`
+
+## Example
+```
+xobs = randn(10_000)
+u = [0.025, 0.5, 0.975]
+Bernstein_cuantiles(u, xobs)
+```
+"""
+function Bernstein_cuantiles(u::Vector{<:Real}, xobs::Vector{<:Real})
+    n = length(xobs)
+    xord = sort(xobs)
+    xp = (xord[1:(n-1)] .+ xord[2:n]) ./ 2
+    xp = vcat(xord[1], xp, xord[n])
+    function g(z)
+        b = 0.0
+        B = Binomial(n, z)
+        p(x) = pdf(B, x)
+        for k ∈ 0:n
+            b += xp[k+1] * p(k)
+        end
+        return b
+    end
+    m = length(u)
+    cuantiles = zeros(m)
+    for i ∈ 1:m
+        cuantiles[i] = g(u[i])
+    end
+    return cuantiles
+end
+
+# Bernstein
+"""
+    Bernstein(x::Vector{<:Real}, xobs::Vector{<:Real}, ε = 0.000001)
+
+Numerical inverse of Bernstein polynomials approximation for a continuous distribution for given values in a
+vector `x` based on an observed random sample given by vector `xobs`, searching over the open interval `]ε,1-ε[`
+
+Dependencies:
+- internal: `Bernstein_cuantiles`
+- external: `Distributions.jl`
+
+## Example
+```
+xobs = randn(10_000)
+x = [-1.96, 0, 1.96]
+Bernstein(x, xobs)
+```
+"""
+function Bernstein(x::Vector{<:Real}, xobs::Vector{<:Real}, ε = 0.000001)
+    n = length(xobs)
+    xord = sort(xobs)
+    xp = (xord[1:(n-1)] .+ xord[2:n]) ./ 2
+    xp = vcat(xord[1], xp, xord[n])
+    g(u) = Bernstein_cuantiles([u], xord)[1] - xdada
+    m = length(x)
+    b = zeros(m)
+    for i ∈ 1:m
+        global xdada = x[i]
+        b[i] = biseccion(g, ε, 1-ε).raíz
+    end
+    return b 
+end
+
 
 ### Table of contents
 
 function Stats3()
     println("Stats3.jl")
     println("=========")
-    println("Main functions: Fn  Tn  cuantil  GoF")
+    println("Main functions: Fn  Tn  Bn  cuantil_puntual  GoF  poligonal  poligonal_cuantiles  Bernstein_cuantiles")
+    println("Auxiliary: EDA  biseccion")
     println("Table of contents: Stats3()")
 end
 ;
